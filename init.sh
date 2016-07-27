923326732a8f7c5f573a53bad64f33435a69a8b1
Steve Androulakis <steve.androulakis@gmail.com>
2016-07-27 21:21:40 +1000

    added init script in case its needed..

---
+++ init.sh
@@ -0,0 +1,3 @@
+git annex init
+git annex initremote archive type=external externaltype=rclone target=steve-annex-test prefix=git-annex chunk=50MiB encryption=none rclone_layout=mixed
+git annex enableremote archive
