21232bce5c5ffe560bb4d54922b43e7420685172
Steve Androulakis <steve.androulakis@gmail.com>
2016-07-27 21:24:50 +1000

    didn't need init

--- init.sh
+++
@@ -1,3 +0,0 @@
-git annex init
-git annex initremote archive type=external externaltype=rclone target=steve-annex-test prefix=git-annex chunk=50MiB encryption=none rclone_layout=mixed
-git annex enableremote archive
